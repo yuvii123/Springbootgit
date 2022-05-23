39bd3c6d1cdbce7d6d4e4f67569dd4889ea5bed7
yuvii123 <yuvraj085808@gmail.com>
2022-05-23 19:54:18 +0530

    initial commit

---
+++ SpringBootGit/SpringBootGit/src/main/java/com/csi/SpringBootGitApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitApplication.class, args);
+	}
+
+}
---
+++ SpringBootGit/SpringBootGit/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+    @GetMapping("/hello")
+    public  String sayHello(){
+        return "Welcome";
+    }
+
+}
